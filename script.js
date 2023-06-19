1c6a29de7fa799baf8d6f606d433db70be46dce1
DK <imamovabdulaziz2000@gmail.com>
2023-06-19 22:58:34 +0500

    ДЗ-1_Js

---
+++ script.js
@@ -0,0 +1,36 @@
+//////////////////////////
+var _1=('1ый ')
+var _2=('2ый ')
+var _3=('3ый ')
+var _4=('4ый ')
+var _5=('5ый ')
+//////////////////////////
+var example=('Пример : ')
+var answer=('  Ваш ответ ')
+//////////////////////////
+var prb1=("1+1= ")
+var prb2=("1000-7= ")
+var prb3=("19*3= ")
+var prb4=("225/15= ")
+var prb5=("7%3= ")
+///////*-+-+-+-*//////////
+var name=prompt('Как вас зовут?')
+var age =+prompt('Сколько вам лет?')
+///////************///////
+console.log('Имя: '+ name);
+console.log('Возраст: '+age);
+var y_answ1=+prompt(prb1+'?')
+var y_answ2=+prompt(prb2+'?')
+var y_answ3=+prompt(prb3+'?')
+var y_answ4=+prompt(prb4+'?')
+var y_answ5=+prompt(prb5+'?')
+///////////////////////////
+console.log(_1+example+prb1+(1+1)+answer+y_answ1);
+console.log(_2+example+prb2+(1000-7)+answer+y_answ2);
+console.log(_3+example+prb3+(19*3)+answer+y_answ3);
+console.log(_4+example+prb4+(225/15)+answer+y_answ4);
+console.log(_5+example+prb5+(7%3)+answer+y_answ5);
+/////////////////////////////////
+
+
+
